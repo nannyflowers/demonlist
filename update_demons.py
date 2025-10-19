5f9c94b7d8ed1eabd8b6b425682f18cc79416e6b
nannyflowers <hayden.nundy.28@gmail.com>
2025-10-19 18:01:40 +0100

    update json

---
+++ update_demons.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json
+import os
+
+excel_file = "GD Demon List.xlsx"
+json_file = "demons.json"
+
+df = pd.read_excel(excel_file)
+columns_to_keep = ["Name", "id"]
+df = df[columns_to_keep]
+
+demons = df.to_dict(orient="records")
+
+with open(json_file, "w") as f:
+    print(demons)
+    json.dump(demons, f, indent=4, ensure_ascii=False)
