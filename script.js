c99e196422001f8285f2b5dfd451a96ed88f0c7a
nannyflowers <hayden.nundy.28@gmail.com>
2025-10-19 19:36:43 +0100

    fix image

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ fetch('demons.json?t=' + new Date().getTime())
             card.innerHTML = `
                 <!-- Left Image -->
                 <div class="demon-image-wrapper">
-                    <img class="demon-image" src="assets\\thenightmare.jpg" alt="${demon.name}">
+                    <img class="demon-image" src="assets/thenightmare.jpg" alt="${demon.name}">
                 </div>
 
                 <!-- Right Content -->
