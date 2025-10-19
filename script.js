391d19523e161b82526cdabdf37ae5183a55f3e4
nannyflowers <hayden.nundy.28@gmail.com>
2025-10-19 14:28:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('demons.json')
     .then(response => response.json())
     .then(demons => {
         // Sort demons by gddlRating descending
-        demons.sort((a, b) => b.gddlRating - a.gddlRating);
+        demons.sort((a,b) => b.gddlRating - a.gddlRating);
 
         const container = document.getElementById('demon-list');
 
@@ -11,13 +11,15 @@ fetch('demons.json')
             card.className = 'demon-card';
 
             card.innerHTML = `
-                <!-- Left Demon Image -->
-                <img class="demon-image" src="assets/thenightmare.jpg" alt="${demon.name}">
+                <!-- Left Image -->
+                <div class="demon-image-wrapper">
+                    <img class="demon-image" src="${demon.image}" alt="${demon.name}">
+                </div>
 
                 <!-- Right Content -->
                 <div class="demon-content">
                     <div class="demon-title">#${index + 1} - ${demon.name}</div>
-
+                    
                     <div class="demon-info-bar">
                         <div>Attempts: ${demon.attempts}</div>
                         <div>GDDL Rating: ${demon.gddlRating}</div>
@@ -31,4 +33,4 @@ fetch('demons.json')
             container.appendChild(card);
         });
     })
-    .catch(error => console.error("Error loading demons:", error));
+    .catch(err => console.error("Error loading demons:", err));
