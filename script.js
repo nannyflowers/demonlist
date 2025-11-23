796d37ecbacfe15649fdbd7c23d91d049a4345dc
nannyflowers <hayden.nundy.28@gmail.com>
2025-11-23 22:56:24 +0000

    time machine

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const date_input = document.getElementById("date-input")
+date_input.valueAsDate = new Date();
+
 fetch('demons.json?t=' + new Date().getTime())
     .then(response => response.json())
     .then(demons => {
@@ -7,9 +10,18 @@ fetch('demons.json?t=' + new Date().getTime())
         const container = document.getElementById('demon-list');
 
 
-        function renderDemons() {
+        function renderDemons(simulated_date) {
             container.innerHTML = '';
-            demons.forEach((demon, index) => {
+            var index = 1
+            demons.forEach((demon) => {
+
+                const time_since_epoch = new Date(demon.dateBeaten).getTime() * 1000;
+                const simulated_time = simulated_date.getTime();
+
+                if (time_since_epoch > simulated_time) {
+                    return
+                }
+
                 const card = document.createElement('div');
                 card.className = 'demon-card';
 
@@ -25,13 +37,6 @@ fetch('demons.json?t=' + new Date().getTime())
                         });
                 });
 
-
-                card.addEventListener("mouseenter", () => {
-                    const audio = new Audio("assets/hover_click.ogg")
-                    audio.volume = 0.02;
-                    audio.play();
-                });
-
                 const timestamp = new Date(demon.dateBeaten * 1000)
                 const formatted_ts = new Intl.DateTimeFormat("de-DE", {
                     day: "2-digit",
@@ -39,12 +44,18 @@ fetch('demons.json?t=' + new Date().getTime())
                     year: "2-digit"
                 }).format(timestamp);
 
+                card.addEventListener("mouseenter", () => {
+                    const audio = new Audio("assets/hover_click.ogg")
+                    audio.volume = 0.02;
+                    audio.play();
+                });
+
                 card.innerHTML = `
                     <div class="demon-image-wrapper">
                         <img class="demon-image" src="https://levelthumbs.prevter.me/thumbnail/${demon.id}" alt="${demon.name}">
                     </div>
                     <div class="demon-content">
-                        <div class="demon-title">#${index + 1} - ${demon.name}</div>
+                        <div class="demon-title">#${index} - ${demon.name}</div>
                         <div class="demon-info-bar">
                             <span>Attempts: ${demon.attempts}</span>
                             <span>GDDL Rating: ${demon.gddlRating}</span>
@@ -56,6 +67,7 @@ fetch('demons.json?t=' + new Date().getTime())
                     </div>
                 `;
                 container.appendChild(card);
+                index += 1
             });
         }
 
@@ -82,32 +94,46 @@ fetch('demons.json?t=' + new Date().getTime())
             };
         }
 
-        function render_with_sort(sort_val, asc = false) {
+        function render_with_sort(date, sort_val, asc = false) {
             sort(sort_val, asc);
-            renderDemons();
+            renderDemons(date);
+        };
+
+        function get_date() {
+            const year = date_input.valueAsDate.getFullYear();
+            const month = date_input.valueAsDate.getMonth();
+            const day = date_input.valueAsDate.getDate();
+            const date = new Date(year, month, day);
+            console.log(date)
+            return date;
         };
 
 
-        render_with_sort("gddlRating")
+        render_with_sort(new Date(), "gddlRating")
 
-        // --- now safe to use demons here ---
         const sortSelect = document.getElementById('sort');
+        var sortBy = sortSelect.value;
+        var date = get_date();
+
         sortSelect.addEventListener('change', () => {
-            const sortBy = sortSelect.value;
-            render_with_sort(sortBy, is_ascending())
+            sortBy = sortSelect.value;
+            render_with_sort(date, sortBy, is_ascending())
         });
 
         const sortButton = document.getElementById("sortOrder");
         sortButton.addEventListener("click", () => {
             if (is_ascending()) {
                 sortButton.textContent = "↓"
-                const sortBy = sortSelect.value;
-                render_with_sort(sortBy, false)
             } else {
                 sortButton.textContent = "↑"
-                const sortBy = sortSelect.value;
-                render_with_sort(sortBy, true)
             };
+            render_with_sort(date, sortBy, is_ascending())
+        });
+
+        date_input.addEventListener("change", () => {
+            date = get_date();
+            console.log(date)
+            render_with_sort(date, sortBy, is_ascending())
         });
     })
     .catch(error => console.error("Error loading demons:", error));
